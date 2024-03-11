6ba88cea12cb96fbaadc3c1632b0a1b8cb502b16
dhwpdnr <ink1237@gmail.com>
2024-03-11 10:47:03 +0900

    [feat] user api

--- app/user/urls.py
+++ app/user/urls.py
@@ -5,7 +5,8 @@ URL mapping for user api.
 from django.urls import path
 from .views import (
     CreateUserView,
-    CreateTokenView
+    CreateTokenView,
+    ManageUserView,
 )
 
 app_name = "user"
@@ -13,4 +14,5 @@ app_name = "user"
 urlpatterns = [
     path("create/", CreateUserView.as_view(), name="create"),
     path("token/", CreateTokenView.as_view(), name="token"),
+    path("me/", ManageUserView.as_view(), name="me"),
 ]
